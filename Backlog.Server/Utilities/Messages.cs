c47ad07c88e113731b8e0d1f3e2d5e7ef2881967
Ivo Lekov <lekov.external@intralot.com>
2022-08-31 21:09:19 +0300

    Add utilities folder with message static class and messages.

---
+++ Backlog.Server/Utilities/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace Backlog.Server.Utilities
+{
+    public static class Messages
+    {
+        public static string NotValidServiceProtocolId = "Not a valid service protocol id";
+        public static string Unauthorized ="Not a valid user";
+        public static string StatusWasUpdated = "The status was updated.";
+        public static string NotFoundWithId = "The service protocol {0} not exist.";
+    }
+}
